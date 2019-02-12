f0a7619736b788be1637ff785bfb8018bd3580a2
Daniel Belcher <daniel.belcher.cs@gmail.com>
2019-02-12 11:44:46 -0600

    Added common components to redux

---
+++ reduxbasics/src/components/common/Header.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-unused-vars */
+//import libraries for making a component
+import React from 'react';
+import { Text, View } from 'react-native';
+
+//make a component
+const Header = (props) => {
+	const { textStyle, viewStyle } = styles;
+
+	return (
+		<View style={viewStyle}>
+			<Text style={textStyle}>{props.headerText}</Text>
+		</View> 
+	);
+};
+
+const styles = {
+	textStyle: {
+		fontSize: 20,
+		color: '#FFFFFF'
+	},
+	viewStyle: {
+		backgroundColor: '#999999',
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: 60,
+		elevation: 10,
+		position: 'relative'
+	}
+};
+
+
+//make the component available to other parts of the app
+export {Header};
